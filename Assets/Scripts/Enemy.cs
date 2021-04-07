f29e3916b9f0abcf200e32f36375f393c38f34f6
Kathiravan <tkathiravan1976@gmail.com>
2021-04-08 00:12:59 +0530

    Adding Enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 4.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movingEnemy();
+    }
+    private void movingEnemy()
+    {
+        transform.Translate(Vector3.down * _speed*Time.deltaTime);
+        if(transform.position.y <= -5.5)
+        {
+            transform.position = new Vector3(Random.Range(-9.5f,9.5f),7.0f,0);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //if enemy hits player
+        //damage player
+        // destroy us
+        if (other.tag == "Player")
+        {
+
+
+
+            // damage Player -lives system
+            PlayerMovement player = other.transform.GetComponent<PlayerMovement>();
+            if (player != null)
+            {
+
+                player.damagePlayer();
+
+            }
+            Destroy(this.gameObject);
+        }
+        // if other is laser
+        // destroy laser
+        // Destroy us
+        if (other.tag == "Laser")
+        {
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+
+        }
+    }
+
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,31 +5,45 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
-    private float _speed = 5.0f;
+    private float _speed = 6.0f;
+    [SerializeField]
+    private GameObject _laser;
+    [SerializeField]
+    private float fireRate = 0.2f;
+    private float nextFire = 0.0f;
+    
+    private int _lives = 3;
     // Start is called before the first frame update
     void Start()
     {
         // to get the position
-        //
-        Debug.Log(transform.position);
+       
     }
 
     // Update is called once per frame
     void Update()
     {
+       
+        movementPlayer();
+        shooting();
+        
+    }
+    private void movementPlayer()
+    {
         float xInput = Input.GetAxis("Horizontal");
         float yInput = Input.GetAxis("Vertical");
-        Vector3 direction = new Vector3(xInput , yInput , 0);
+        Vector3 direction = new Vector3(xInput, yInput, 0);
         transform.Translate(direction * _speed * Time.deltaTime);
-        if(transform.position.y >= 0)
+        if (transform.position.y >= 0)
         {
             transform.position = new Vector3(transform.position.x, 0, transform.position.z);
-        }else if(transform.position.y <= -3.9)
+        }
+        else if (transform.position.y <= -3.9)
         {
             transform.position = new Vector3(transform.position.x, -3.9f, transform.position.z);
         }
 
-        if(transform.position.x >= 10.0)
+        if (transform.position.x >= 10.0)
         {
             transform.position = new Vector3(-10.0f, transform.position.y, transform.position.z);
         }
@@ -38,4 +52,29 @@ public class PlayerMovement : MonoBehaviour
             transform.position = new Vector3(10.0f, transform.position.y, transform.position.z);
         }
     }
+    private void shooting()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(_laser,transform.position+new Vector3(0,0.5f,0),Quaternion.identity);
+        }
+    }
+
+    public void damagePlayer()
+    {
+        Debug.Log("_lives before subtract: " + _lives);
+        _lives--;
+        Debug.Log("_lives after subtract: " + _lives);
+
+        // check if dead
+        // destroy us
+        if (_lives < 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+
+
 }
